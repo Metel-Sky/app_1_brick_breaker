eca006ce716368ca2a39e3c04a1d2e36910dff42
djkif <djkif1212@gmail.com>
2024-10-10 08:59:01 +0300

    google courses app 1

--- lib/src/brick_breaker.dart
+++ lib/src/brick_breaker.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math' as math;
 
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
@@ -6,7 +7,7 @@ import 'package:flame/game.dart';
 import 'components/components.dart';
 import 'config.dart';
 
-class BrickBreaker extends FlameGame {
+class BrickBreaker extends FlameGame with HasCollisionDetection{
   BrickBreaker()
       : super(
           camera: CameraComponent.withFixedResolution(
@@ -14,17 +15,29 @@ class BrickBreaker extends FlameGame {
             height: gameHeight,
           ),
         );
+
+  final rand = math.Random();
+
   double get width => size.x;
+
   double get height => size.y;
 
   @override
-  FutureOr<void> onLoad()async{
+  FutureOr<void> onLoad() async {
     super.onLoad();
 
     camera.viewfinder.anchor = Anchor.topLeft;
 
     world.add(PlayArea());
+
+    world.add(
+      Ball(
+          radius: ballRadius,
+          position: size / 2,
+          velocity: Vector2((rand.nextDouble() - 0.5) * width, height * 0.2)
+              .normalized()
+            ..scale(height / 4)),
+    );
+    debugMode = true;
   }
-
 }
-
